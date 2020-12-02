8822138ef0a14f4aa5de01fee3121562d4e5ce2b
Sam Sheets <sam.sheets94@gmail.com>
2020-12-02 13:56:14 -0700

    Tabs component

--- src/App.js
+++ src/App.js
@@ -8,8 +8,16 @@ import Counter from './state/Counter';
 import HelloWorld from './state-drills/HelloWorld';
 import Bomb from './state-drills/Bomb';
 import RouletteGun from './state-drills/RouletteGun';
+import Tabs from './state/Tabs';
 
-
+const tabsProp = [
+  { name: 'First tab',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque.' },
+  { name: 'Second tab',
+    content: 'Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit.' },
+  { name: 'Third tab',
+    content: 'Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur.' },
+];
 
 const firstTooltip = (
   <Tooltip color='hotpink' message='tooltip message'>
@@ -26,6 +34,7 @@ const secondTooltip = (
 function App() {
   return (
     <main className='App'>
+      <Tabs tabs={tabsProp} />
       <RouletteGun />
       <Bomb />
       <Split className='left' flexBasis={2}>
@@ -41,7 +50,7 @@ function App() {
       <Messages name="Messages" unread={0}/>
       <Messages name="Notifications" unread={10}/>
       
-      <TheDate />
+      
       <Counter count={123}/>
       <HelloWorld />
     </main>
