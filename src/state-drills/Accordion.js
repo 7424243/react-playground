01daca373c2850c30299b8b9e1f572c76dee4ac4
Sam Sheets <sam.sheets94@gmail.com>
2020-12-03 10:37:54 -0700

    accurate naming of renderListItem

--- src/state-drills/Accordion.js
+++ src/state-drills/Accordion.js
@@ -11,11 +11,11 @@ class Accordion extends React.Component {
         this.setState({currentIndex: sectionIndex})
     }
 
-    renderListItems(section, idx, currentIndex) {
+    renderListItem(section, idx, currentIndex) {
         return (
             <li key={idx}>
                 <button onClick={() => this.handleSetActiveSection(idx)}>{section.title}</button>
-                {(currentIndex === idx) && <p>{section.content}</p>}
+                {(currentIndex === idx) ? <p>{section.content}</p> : null}
             </li>
         )
     }
@@ -27,7 +27,7 @@ class Accordion extends React.Component {
         return (
             <ul>
                 {sections.map((section, idx) =>
-                   this.renderListItems(section, idx, currentIndex)
+                   this.renderListItem(section, idx, currentIndex)
                 )}
             </ul>
         )
